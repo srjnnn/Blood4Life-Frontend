b653330b6e36560fe43a43cd6618d92be085658a
Srjnn <mrsrjn224@gmail.com>
2025-07-25 23:40:15 +0545

    validateAuthToken

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Signup from './pages/Signup'
 import BloodGroup from './pages/BloodGroup'
-
+import apiRequest from './utils/api.js'
+import { apiRoutes } from './utils/globalConstraints.js'
 function App() {
+  const [isLoggedin, setisLoggedin]=useState(null);
+  useEffect(()=>{
+    const checkAuthStatus = async()=>{
+      // search if the token is valid
+      const authToken = localStorage.getItem("token");
+      // send the userAuthToken
+      const payload = {
+        token : authToken
+      }
+      const response = await apiRequest(apiRoutes.auth.validate,"POST",payload);
+      setisLoggedin(!!response);
+    }
+  })
 
   return (
     <>
